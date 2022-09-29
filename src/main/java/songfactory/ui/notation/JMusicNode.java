1685ef89bb30dbfaca2a13cff83121a837f8c08b
Luke Walter <59621677+LukeWalter@users.noreply.github.com>
2022-09-28 22:50:23 -0400

    Removed excess images and created static references

--- src/main/java/songfactory/ui/notation/JMusicNode.java
+++ src/main/java/songfactory/ui/notation/JMusicNode.java
@@ -6,6 +6,23 @@ import javax.swing.*;
 public abstract class JMusicNode extends JComponent {
 
     protected static Image trebleClefImage = (new ImageIcon("res/trebleClef.png")).getImage();
+    protected static Image commonTimeImage = (new ImageIcon("res/commonTime.png")).getImage();
+
+    protected static Image flatImage = (new ImageIcon("res/flat.png")).getImage();
+    protected static Image sharpImage = (new ImageIcon("res/sharp.png")).getImage();
+    protected static Image naturalImage = (new ImageIcon("res/natural.png")).getImage();
+
+    protected static Image sixteenthNoteImage = (new ImageIcon("res/sixteenthNote.png")).getImage();
+    protected static Image eighthNoteImage = (new ImageIcon("res/eighthNote.png")).getImage();
+    protected static Image quarterNoteImage = (new ImageIcon("res/quarterNote.png")).getImage();
+    protected static Image halfNoteImage = (new ImageIcon("res/halfNote.png")).getImage();
+    protected static Image wholeNoteImage = (new ImageIcon("res/wholeNote.png")).getImage();
+
+    protected static Image sixteenthRestImage = (new ImageIcon("res/sixteenthRest.png")).getImage();
+    protected static Image eightRestImage = (new ImageIcon("res/eighthRest.png")).getImage();
+    protected static Image quarterRestImage = (new ImageIcon("res/quarterRest.png")).getImage();
+    protected static Image halfRestImage = (new ImageIcon("res/halfRest.png")).getImage();
+    protected static Image wholeRestImage = (new ImageIcon("res/wholeRest.png")).getImage();
 
     protected int xOffset;
     protected int yOffset;
