fe0f4acdc4e0a2c0349b6aef26481fe024f22747
Luke Walter <59621677+LukeWalter@users.noreply.github.com>
2022-09-30 21:21:52 -0400

    Documentation update #2.1

--- src/main/java/songfactory/ui/notation/QuarterNote.java
+++ src/main/java/songfactory/ui/notation/QuarterNote.java
@@ -2,8 +2,14 @@ package songfactory.ui.notation;
 
 import java.awt.Dimension;
 
+/**
+ * Quarter note symbol.
+ */
 public class QuarterNote extends JNote {
 
+    /**
+     * Quarter note constructor.
+     */
     public QuarterNote() {
         super(QUARTER_NOTE, 7, 35, new Dimension(20, 12));
 
